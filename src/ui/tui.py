a78734252cd27ab22f0738d01e9bda50b9430df7
Kristofers Solo <kristians.cagulis@pm.me>
2023-11-01 19:38:12 +0200

    refactor(tui): refactor `create_books_table`

--- src/ui/tui.py
+++ src/ui/tui.py
@@ -3,26 +3,18 @@ from bookstore.book import Book
 from bookstore.inventory import Inventory
 
 from .handlers import exit_on_q
+
 from .widgets.table import Table
 
 
-FOCUS_STYLE = urwid.AttrMap("default", "dark red", "standout")
-UNFOCUS_STYLE = urwid.AttrMap("default", "default", "standout")
+def create_row(cells) -> urwid.Columns:
+    return urwid.Columns([urwid.Text(str(cell)) for cell in cells])
 
 
 def create_books_table(books: list[Book]) -> Table:
-    header = urwid.Columns([urwid.Text(header) for header in Book.fields()])
-    header = urwid.AttrMap(header, "header", focus_map=FOCUS_STYLE)
-
-    rows = [header]
-
-    for book in books:
-        row = urwid.Columns([urwid.Text(str(value)) for value in book.field_values()])
-        rows.append(urwid.AttrMap(row, "body", focus_map=FOCUS_STYLE))
-
-    walker = urwid.SimpleListWalker(rows)
-    books_table = Table(walker)
-    return books_table
+    header = create_row(Book.fields())
+    rows = [create_row(book.field_values()) for book in books]
+    return Table(urwid.SimpleListWalker([header] + rows))
 
 
 def render(inventory: Inventory):
